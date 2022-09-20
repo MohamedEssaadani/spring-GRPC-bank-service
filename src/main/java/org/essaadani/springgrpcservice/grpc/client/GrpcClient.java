171fb5befc31d8d4d8a9f6d51731c4bb0410ea5d
Mohamed Es-saadani <essaadani80@gmail.com>
2022-09-20 21:25:16 +0100

    java GRPC async client

--- src/main/java/org/essaadani/springgrpcservice/grpc/client/GrpcClient.java
+++ src/main/java/org/essaadani/springgrpcservice/grpc/client/GrpcClient.java
@@ -2,11 +2,14 @@ package org.essaadani.springgrpcservice.grpc.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.essaadani.springgrpcservice.grpc.stub.Bank;
 import org.essaadani.springgrpcservice.grpc.stub.BankServiceGrpc;
 
+import java.io.IOException;
+
 public class GrpcClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // connect to service
         ManagedChannel managedChannel = ManagedChannelBuilder
                 .forAddress("localhost", 9999)
@@ -30,5 +33,28 @@ public class GrpcClient {
         System.out.println("From :"+response.getCurrencyFrom());
         System.out.println("To :"+ response.getCurrencyTo());
         System.out.println("Result: "+ response.getConversionResult()+" MAD");
+
+
+        BankServiceGrpc.BankServiceStub bankServiceStub=BankServiceGrpc.newStub(managedChannel);
+        bankServiceStub.convertCurrency(request, new StreamObserver<Bank.ConvertCurrencyResponse>() {
+            @Override
+            public void onNext(Bank.ConvertCurrencyResponse response) {
+                System.out.println("************************NON BLOCKING RESULT********************");
+                System.out.println("From :"+response.getCurrencyFrom());
+                System.out.println("To :"+ response.getCurrencyTo());
+                System.out.println("Result: "+ response.getConversionResult()+" MAD");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("ECHANGE ENDDD!!");
+            }
+        });
+        System.in.read();
     }
 }
